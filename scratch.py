36d06549e7fa3311766decf42eeb14c28ff461c4
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-09-18 16:01:54 +0430

    practicing turtle

---
+++ scratch.py
@@ -0,0 +1,18 @@
+import turtle
+
+myturtle = turtle.Turtle()
+
+# go to a certian coordinate
+myturtle.penup()
+myturtle.goto(50, 75)
+
+myturtle.pendown()
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+myturtle.left(90)
+myturtle.forward(100)
+myturtle.left(90)
+myturtle.forward(200)
+
+turtle.done()
