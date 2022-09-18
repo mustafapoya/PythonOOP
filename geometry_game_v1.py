e3f4fa7777d7a01566e7da527a795818a873f8bf
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-09-18 16:13:16 +0430

    reformat code, add point drawing

--- geometry_game_v1.py
+++ geometry_game_v1.py
@@ -38,23 +38,31 @@ class GuiRectangle(Rectangle):
         canvas.left(90)
         canvas.forward(self.point2.y - self.point1.y)
 
-        turtle.done()
 
+class GuiPoint(Point):
+    def draw(self, canvas: turtle.Turtle, size=5, color='red'):
+        canvas.penup()
+        canvas.goto(self.x, self.y)
+        canvas.pendown()
+        canvas.dot(size, color)
+
+
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)), Point(randint(10, 19), randint(10, 19)))
+
+print("Rectangle Coordinates: ", rectangle.point1.x, ",", rectangle.point1.y, \
+      "and", rectangle.point2.x, ",", rectangle.point2.y)
+
+user_point = GuiPoint(float(input("Guess X: ")), float(input("Guess Y: ")))
+
+user_area = float(input("Guess rectangle area: "))
+
+print("your area was off by: ", rectangle.area() - user_area)
+
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
 
 gui_rectangle = GuiRectangle(Point(randint(0, 400), randint(0, 400)), Point(randint(10, 400), randint(10, 400)))
 myturtle = turtle.Turtle()
 gui_rectangle.draw(canvas=myturtle)
+user_point.draw(canvas=myturtle)
 
-
-
-# rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)), Point(randint(10, 19), randint(10, 19)))
-#
-# print("Rectangle Coordinates: ", rectangle.lowleft.x, ",", rectangle.lowleft.y, "and", rectangle.upright.x, ",", rectangle.upright.y)
-#
-# user_point = Point(float(input("Guess X: ")), float(input("Guess Y: ")))
-#
-# user_area = float(input("Guess rectangle area: "))
-#
-# print("your area was off by: ", rectangle.area() - user_area)
-#
-# print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+turtle.done()
