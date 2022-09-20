451a3a04442300977e5bbafa75250baa79d4439e
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-09-20 17:04:12 +0430

    implementing oop rules in Math Painting

---
+++ math_painting/array_to_numpy.py
@@ -0,0 +1,13 @@
+import numpy as np
+from PIL import Image
+
+# Create 3d numpy array of zeros, then replace zeros (black pixels) with yellow pxls
+data = np.zeros((5, 4, 3), dtype=np.uint8)
+data[:] = [255, 255, 0]
+print(data)
+
+data[0:3, 0:2] = [255, 200, 233]
+data[3:4, 1:4] = [45, 3, 233]
+
+img = Image.fromarray(data, 'RGB')
+img.save('canvas.png')
