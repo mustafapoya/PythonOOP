cb4e0a2c6158233d8c87aab1a6068f6756db8b2c
Mustafa Poya <mustafapoyaabc@gmail.com>
2022-09-21 17:11:33 +0430

    add wikipedia, download image and display

--- photo_searcher/main.py
+++ photo_searcher/main.py
@@ -1,14 +1,33 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+import wikipedia
+import requests
 
 
 Builder.load_file('frontend.kv')
 
 
 class FirstScreen(Screen):
-    def search_image(self):
-        pass
+    def get_image_link(self):
+        # get user query from textInput
+        query = self.manager.current_screen.ids.user_query.text
+        # get wikipedia page and first image link
+        page = wikipedia.page(query)
+        image_link = page.images[0]
+        return image_link
+
+    def download_image(self):
+        req = requests.get(self.get_image_link())
+        image_path = 'files/image.jpg'
+        with open(image_path, 'wb') as file:
+            file.write(req.content)
+
+        return image_path
+
+    def set_image(self):
+        # set the image in the image widget
+        self.manager.current_screen.ids.img.source = self.download_image()
 
 
 class RootWidget(ScreenManager):
